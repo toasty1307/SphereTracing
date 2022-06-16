be6174f6233be82cf92f4582661432587e2316e2
toasty <toastyboialt@gmail.com>
2022-06-16 22:28:35 +0530

    stuff

--- SphereTracing/Program.cs
+++ SphereTracing/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using ComputeSharp;
+using SixLabors.ImageSharp;
+using SphereTracing.Shaders;
+using Rgba32 = SixLabors.ImageSharp.PixelFormats.Rgba32;
+
+namespace SphereTracing
+{
+    public class Program
+    {
+        public const int Width = 1920;
+        public const int Height = 1080;
+
+        private static void Main()
+        {
+            Console.WriteLine("Starting...");
+            var sw = Stopwatch.StartNew();
+            
+            var array = new uint[Width * Height];
+
+            using var buffer = GraphicsDevice.Default.AllocateReadWriteBuffer(array);
+            
+            GraphicsDevice.Default.For(Width, Height, new SphereGameShader(buffer));
+
+            buffer.CopyTo(array);
+            
+            sw.Stop();
+            Console.WriteLine($"Done! The render took {sw.Elapsed.TotalSeconds} seconds.\nSaving Image...");
+            
+            var image = new Image<Rgba32>(Width, Height);
+            var pixels = Unsafe.As<uint[], Rgba32[]>(ref array);
+            image.Frames.AddFrame(pixels);
+            image.Frames.RemoveFrame(0);
+            image.SaveAsPng("output.png");
+            
+            Console.WriteLine("Saved Image! Press any key to open image and exit.");
+            Console.ReadKey();
+            
+            Process.Start(new ProcessStartInfo("output.png"){UseShellExecute = true});
+        }
+    }
+}
--- SphereTracing/Shaders/SphereGameShader.cs
+++ SphereTracing/Shaders/SphereGameShader.cs
@@ -1,6 +1,34 @@
-﻿namespace SphereTracing.Shaders;
+﻿using ComputeSharp;
+// ReSharper disable All
 
-public class SphereGameShader
+namespace SphereTracing.Shaders;
+
+[AutoConstructor]
+public readonly partial struct SphereGameShader : IComputeShader
 {
+    private const int Width = Program.Width;
+    private const int Height = Program.Height;
+
+    public readonly ReadWriteBuffer<uint> buffer;
+
+    [ShaderMethod]
+    public static bool OnSphere(Float2 center, float radius, Float2 xy) =>
+        Hlsl.Distance(xy, center) <= radius;
     
+    public void Execute()
+    {
+        var pixel = ThreadIds.Y * Width + ThreadIds.X;
+        
+        var center = new Float2(Width / 2, Height / 2);
+        int radius = 100;
+        var red = 0;
+        var green = 0;
+        var blue = 0;
+        if (OnSphere(center, radius, ThreadIds.XY))
+        {
+            red = blue = green = 255;
+        }
+
+        buffer[pixel] = (uint)((255 << 24 | blue << 16 | green << 8 | red) & 0xFFFFFFFF);
+    }
 }
